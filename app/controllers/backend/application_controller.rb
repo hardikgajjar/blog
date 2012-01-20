e3b71cddd7ee6d3efa56b1c4aacb81e3da5738bc
Hardik Gajjar <hardik.kwt@gmail.com>
2012-01-20 12:43:13 +0530

    finished.

--- app/controllers/backend/application_controller.rb
+++ app/controllers/backend/application_controller.rb
@@ -1,4 +1,11 @@
 class Backend::ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_backend_admin!
+  
+  def sa
+    @user = current_user
+    # raise params.inspect
+    if @user.update_with_password(params[:user])
+
+  end
 end
